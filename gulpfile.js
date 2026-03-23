836471d1a89800e0506eb8d16c4a59f32cc93662
nhall <nick@fastspot.com>
2026-03-22 21:15:16 -0400

    chore: update dependencies in package.json and improve twig template for social links

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,61 @@ const browserSync = require('browser-sync').create();
 const del = require('del');
 const gulpif = require('gulp-if');
 const argv = require('yargs').argv;
-const twig = require('gulp-twig');
-const htmlbeautify = require('gulp-html-beautify');
+const through = require('through2');
+const prettier = require('gulp-prettier').default;
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
-const cssnano = require('gulp-cssnano');
+const cssnano = require('cssnano');
 const rename = require('gulp-rename');
 const packageJSON = require('./package.json');
 const dataJSON = require('./data.json');
 
+dataJSON.sameAs = JSON.stringify(
+	dataJSON.links
+		.filter(link => link.name !== 'Resume')
+		.map(link => link.url)
+);
 
-function compileTwig() {
+
+function compileTemplate(data) {
+	return through.obj(function (file, enc, cb) {
+		let html = file.contents.toString();
+
+		// {{ variable }}
+		html = html.replace(/\{\{\s*(\w+)\s*\}\}/g, (match, key) => {
+			return data[key] !== undefined ? data[key] : match;
+		});
+
+		// {% for item in array %} ... {% endfor %}
+		html = html.replace(/\{%\s*for\s+(\w+)\s+in\s+(\w+)\s*%\}([\s\S]*?)\{%\s*endfor\s*%\}/g, (match, item, arr, body) => {
+			if (!Array.isArray(data[arr])) return '';
+			return data[arr].map((entry, i) => {
+				let result = body;
+				// {{ item.property }}
+				result = result.replace(/\{\{\s*\w+\.(\w+)\s*\}\}/g, (m, prop) => {
+					return entry[prop] !== undefined ? entry[prop] : m;
+				});
+				// {% if item.property != 'value' %} ... {% endif %}
+				result = result.replace(/\{%\s*if\s+\w+\.(\w+)\s*!=\s*'([^']*)'\s*%\}([\s\S]*?)\{%\s*endif\s*%\}/g, (m, prop, val, content) => {
+					return entry[prop] !== val ? content : '';
+				});
+				// {% if item.property == 'value' %} ... {% endif %} (not used but safe to support)
+				result = result.replace(/\{%\s*if\s+\w+\.(\w+)\s*==\s*'([^']*)'\s*%\}([\s\S]*?)\{%\s*endif\s*%\}/g, (m, prop, val, content) => {
+					return entry[prop] === val ? content : '';
+				});
+				return result;
+			}).join('');
+		});
+
+		file.contents = Buffer.from(html);
+		cb(null, file);
+	});
+}
+
+
+function compileHTML() {
 	return src('src/twig/*.twig')
-		.pipe(twig({
-			data: dataJSON
-		}))
+		.pipe(compileTemplate(dataJSON))
 		.pipe(rename({
 			extname: '.html'
 		}))
@@ -28,10 +68,11 @@ function compileTwig() {
 
 function prettyhtml() {
 	return src('*.html')
-		.pipe(htmlbeautify({
-			"indent_size": 1,
-			"indent_char": "	",
-			"preserve_newlines": false
+		.pipe(prettier({
+			parser: 'html',
+			useTabs: true,
+			tabWidth: 1,
+			printWidth: 120
 		}))
 		.pipe(dest('./'));
 }
@@ -58,9 +99,9 @@ function compileSass() {
 				],
 				replace: false,
 				rootValue: 16
-			})
+			}),
+			cssnano()
 		])))
-		.pipe(gulpif(argv.production, cssnano()))
 		.pipe(dest('css'))
 		.pipe(browserSync.stream());
 }
@@ -107,7 +148,7 @@ function watchFileSystem(done) {
 		'src/twig/**/*.twig',
 		'data.json'
 	], series(
-		compileTwig
+		compileHTML
 	));
 
 	watch('src/css/**/**', series(
@@ -119,13 +160,13 @@ function watchFileSystem(done) {
 
 
 exports.compileSass = compileSass;
-exports.compileTwig = compileTwig;
+exports.compileHTML = compileHTML;
 exports.clean = clean;
 
 
 exports.default = series(
 	compileSass,
-	compileTwig,
+	compileHTML,
 	watchFileSystem,
 	runBrowserSync
 );
@@ -133,6 +174,6 @@ exports.default = series(
 
 exports.build = series(
 	compileSass,
-	compileTwig,
+	compileHTML,
 	prettyhtml
 );
